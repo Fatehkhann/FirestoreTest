0c1f53549777f28505286076a0704b5b1e65a353
Fateh Khan <fatehkhann@gmail.com>
2021-04-28 22:45:34 +0500

    Initial Commit

---
+++ CityRunner/CityRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityRunnerApp.swift
+//  CityRunner
+//
+//  Created by Fateh Khan on 28/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct CityRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
